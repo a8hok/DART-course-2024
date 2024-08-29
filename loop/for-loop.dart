a5e14821bde3623cbecb0d648801a4f1858d1a57
a8hok <ashokkumar.explore@gmail.com>
2024-08-29 06:37:49 +0530

    Dart tutorial-for loop basic

---
+++ loop/for-loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  for (int i = 0; i < 5; i++) {
+    print("i = $i");
+  }
+  for (int i = 5; i > 0; i--) {
+    print("i = $i");
+  }
+  for (int i = 1; i < 5; i += 2) {
+    print("i = $i");
+  }
+}
