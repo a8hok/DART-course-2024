20b822635df4611cc3fe86f039c432d0ca2b55f7
a8hok <ashokkumar.explore@gmail.com>
2024-08-29 06:23:52 +0530

    Dart tutorial- variables examples added

---
+++ variables/variables-examples.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 25; // Integer
+  double height = 5.9; // Double
+  String name = "Raju"; // String
+  bool isStudent = true; // Boolean
+  var address = "Bangalore"; // Dynamic typing with var
+
+  print(age);
+  print(height);
+  print(name);
+  print(isStudent);
+  print(address);
+}
