5e04cd268bae46c5a94ab8c54e7d7e336b452371
a8hok <ashokkumar.explore@gmail.com>
2024-10-10 07:18:34 +0530

    updated the logic and comments

--- more-example/student-grade.dart
+++ more-example/student-grade.dart
@@ -1,15 +1,27 @@
 import 'dart:io';
 
+// ********RULE OF FUNCTIONS***********
+
+// Call it with a function name
+// Pass the arguments if needed
+// Get the arguments passed if needed
+// Do Manipulations
+// make sure the return type
+// Return it
+// Catch it with a variable name and proper variable type
+
+// ********RULE OF FUNCTIONS***********
+
 List<String> calculateTotal(List<Map<String, dynamic>> studentList) {
-  print(studentList);
   dynamic total = 0;
   List<String> grades = [];
   for (int i = 0; i < studentList.length; i++) {
-    print(studentList[i]);
-    print(studentList.length);
-    print(studentList[i]['score'].length);
-    print(studentList[i]['score']);
-    print(studentList[i]['name']);
+    // print(studentList);
+    // print(studentList[i]);
+    // print(studentList.length);
+    // print(studentList[i]['score'].length);
+    // print(studentList[i]['score']);
+    // print(studentList[i]['name']);
     for (int j = 0; j < studentList[i]['score'].length; j++) {
       print(studentList[i]['score'][j]);
       total = total + studentList[i]['score'][j];
@@ -17,17 +29,11 @@ List<String> calculateTotal(List<Map<String, dynamic>> studentList) {
     double avg = total / 4;
     String grade = calculateGrade(avg);
     grades.add(grade);
+    total = 0;
   }
   return grades;
 }
 
-//Call it with a function name
-//Pass the arguments if needed
-//Get the arguments passed
-//Do Manipulations
-// make sure the return type
-//Return it
-//Catch it with a variable name and proper variable type
 String calculateGrade(double avg) {
   if (avg > 85 && avg <= 100) {
     return ('Grade: A');
@@ -42,24 +48,24 @@ String calculateGrade(double avg) {
   }
 }
 
+// List<String> name = ['bob','Aarush','Agrima'];
+// List<int> name = [5,2,7];
+// List<Map<String,String>> names = [{
+//     'name1' : 'bob',
+//     'name2' : 'Aarush',
+
+// }];
+
 void main() {
   print('How many students do you want to enter?');
   String? studentLength = stdin.readLineSync();
   dynamic studentLengthInt = int.tryParse(studentLength!);
   List<Map<String, dynamic>> stuList = [];
-  // List<String> name = ['bob','Aarush','Agrima'];
-  // List<int> name = [5,2,7];
-  // List<Map<String,String>> names = [{
-  //     'name1' : 'bob',
-  //     'name2' : 'Aarush',
 
-  // }];
-  //print(studentLengthInt == null);
   if (studentLengthInt != null) {
     for (int i = 0; i < studentLengthInt; i++) {
       print('What is your name?');
       String? name = stdin.readLineSync();
-      // print(name);
       print('What is your Math score?');
       String? math = stdin.readLineSync();
       int? mathInt = int.tryParse(math!);
@@ -73,21 +79,15 @@ void main() {
       String? history = stdin.readLineSync();
       int? historyInt = int.tryParse(history!);
 
-      // String finalGrade =
-      //     calculateTotal(mathInt, scienceInt, englishInt, historyInt);
-
-      // print(finalGrade);
-
       Map<String, dynamic> studentDetails = {};
       studentDetails["name"] = name;
-      studentDetails["score"] = <int>[]; // Declare as List<int>
-      // studentDetails["score"].addAll([mathInt, scienceInt, englishInt, historyInt]);
-      // // print(studentDetails);
+      studentDetails["score"] = <int>[];
+
       (studentDetails["score"] as List<int>).addAll(
           [mathInt ?? 0, scienceInt ?? 0, englishInt ?? 0, historyInt ?? 0]);
       stuList.add(studentDetails);
     }
-    // print(stuList);
+
     List<String> grades = calculateTotal(stuList);
     for (int i = 0; i < grades.length; i++) {
       print(grades[i]);
