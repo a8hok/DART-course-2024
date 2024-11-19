17faedff0f447d705706355341aebd598950ceb7
a8hok <ashokkumar.explore@gmail.com>
2024-11-19 08:31:25 +0530

    calc app

---
+++ flutter/calc-app-lib/lib/calc_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Widget calcButton(
+    String buttonText, Color buttonColor, void Function()? buttonPressed) {
+  return Container(
+    width: 75,
+    height: buttonText == '=' ? 150 : 70,
+    padding: const EdgeInsets.all(0),
+    child: ElevatedButton(
+      onPressed: buttonPressed,
+      style: ElevatedButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          backgroundColor: Colors.pink),
+      child: Text(
+        buttonText,
+        style: const TextStyle(fontSize: 13, color: Colors.white),
+      ),
+    ),
+  );
+}
