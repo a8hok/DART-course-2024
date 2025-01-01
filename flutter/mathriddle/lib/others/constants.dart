fa43bb2e31502ba6795cb1ff6930a467462aa7f9
a8hok <ashokkumar.explore@gmail.com>
2025-01-01 08:13:44 +0530

    added math ridlle flutter app

---
+++ flutter/mathriddle/lib/others/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static int minNumber = 2, maxNumber = 99, minTimer = 90, maxTimer = 999;
+
+  static const String MIN_NUMBER_SP = "MIN_NUMBER_SP";
+  static const String MAX_NUMBER_SP = "MAX_NUMBER_SP";
+  static const String MAX_TIMER_SP = "MAX_TIMER_SP";
+  static const String NUM_OF_ROWS_SP = "NUM_OF_ROWS_SP";
+  static const String NUM_OF_COLS_SP = "NUM_OF_COLS_SP";
+}
