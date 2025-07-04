ca977c783cfecf200c18cc2bae5e9abb65b08e7b
a8hok <ashokkumar.explore@gmail.com>
2025-07-04 15:09:24 +0530

    added profile page

--- memoryfinal/lib/screens/profile.dart
+++ memoryfinal/lib/screens/profile.dart
@@ -1,12 +1,141 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:memory_sample/screens/login.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage>
+    with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _mobileController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  File? _imageFile;
+  bool _hasShownToast = false;
+  bool _hasValidationError = false;
+
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _mobileController.text = "9876543210";
+    _passwordController.text = "******";
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
+    _animationController.forward(); // show animation initially
+  }
+
+  void _showToast(String message) {
+    final overlay = Overlay.of(context);
+    final overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: 130, // ~100px from top
+        left: 0,
+        right: 0,
+        child: SlideToast(message: message),
+      ),
+    );
+
+    overlay.insert(overlayEntry);
+
+    Future.delayed(const Duration(seconds: 4), () {
+      overlayEntry.remove();
+    });
+  }
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image =
+        await picker.pickImage(source: ImageSource.gallery, imageQuality: 75);
+
+    if (image != null) {
+      setState(() {
+        _imageFile = File(image.path);
+      });
+    }
+  }
+
+  void _signOut() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginPage()),
+    );
+  }
+
+  void _handleSave() {
+    _hasValidationError = false;
+    _hasShownToast = false;
+    _formKey.currentState!.validate();
+
+    if (!_hasValidationError) {
+      _showToast("Profile updated successfully!");
+      // Add backend update logic if needed
+    }
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+    required String? Function(String?) validator,
+    bool obscureText = false,
+  }) {
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        validator: validator,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          labelText: label,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildReadOnlyField(String label, String value, IconData icon) {
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: TextFormField(
+        initialValue: value,
+        readOnly: true,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          labelText: label,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    final email = user?.email ?? 'example@email.com';
+
     return Scaffold(
       body: Stack(
         children: [
@@ -30,31 +159,121 @@ class ProfilePage extends StatelessWidget {
             left: 20,
             child: IconButton(
               icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+              onPressed: () => Navigator.pop(context),
             ),
           ),
 
           // Main content
           Center(
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
+              padding: const EdgeInsets.symmetric(horizontal: 24),
               child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 60),
-                    Text(
-                      'Profile!',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.lato(
-                        fontSize: 30,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 60),
+
+                      // Title
+                      Text(
+                        'Profile',
+                        style: GoogleFonts.lato(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+
+                      const SizedBox(height: 20),
+
+                      // Profile image upload
+                      GestureDetector(
+                        onTap: _pickImage,
+                        child: ScaleTransition(
+                          scale: _fadeAnimation,
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundColor: Colors.grey[300],
+                            backgroundImage: _imageFile != null
+                                ? FileImage(_imageFile!)
+                                : null,
+                            child: _imageFile == null
+                                ? const Icon(Icons.camera_alt, size: 30)
+                                : null,
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 30),
+
+                      _buildReadOnlyField('Email', email, Icons.email),
+                      const SizedBox(height: 20),
+
+                      _buildTextField(
+                        controller: _mobileController,
+                        label: 'Mobile',
+                        icon: Icons.phone,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            _showToast('Enter mobile number');
+                            _hasValidationError = true;
+                          } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+                            _showToast('Mobile number must be 10 digits');
+                            _hasValidationError = true;
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+
+                      _buildTextField(
+                        controller: _passwordController,
+                        label: 'Password',
+                        icon: Icons.lock,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            _showToast('Enter password');
+                            _hasValidationError = true;
+                          } else if (value.length < 6) {
+                            _showToast(
+                                'Password must be at least 6 characters');
+                            _hasValidationError = true;
+                          }
+                          return null;
+                        },
+                      ),
+
+                      const SizedBox(height: 30),
+
+                      ElevatedButton(
+                        onPressed: _handleSave,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF623AA2),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 14),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          'Save Changes',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      TextButton.icon(
+                        onPressed: _signOut,
+                        icon: const Icon(Icons.logout, color: Colors.redAccent),
+                        label: const Text(
+                          'Sign Out',
+                          style: TextStyle(color: Colors.redAccent),
+                        ),
+                      ),
+                      const SizedBox(height: 60),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -74,6 +293,74 @@ class ProfilePage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SlideToast extends StatefulWidget {
+  final String message;
+
+  const SlideToast({super.key, required this.message});
+
+  @override
+  State<SlideToast> createState() => _SlideToastState();
+}
+
+class _SlideToastState extends State<SlideToast>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(1.5, 0.0), // Start off-screen (right)
+      end: const Offset(0.0, 0.0), // Slide to center
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 6, 136, 30),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 6,
+                offset: Offset(2, 4),
+              ),
+            ],
+          ),
+          child: Text(
+            widget.message,
+            style: const TextStyle(color: Colors.white, fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ),
       ),
     );
   }
