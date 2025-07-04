b64f018fa683c36fd1ba5c62a58ef000ee4446dd
a8hok <ashokkumar.explore@gmail.com>
2025-07-04 15:17:21 +0530

    added firebase setup in main dart file

--- memoryfinal/lib/main.dart
+++ memoryfinal/lib/main.dart
@@ -1,8 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'screens/splash.dart';
 
-void main() {
+// import 'package:firbase_connecting/pages/login/login.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+//   runApp(const MyApp());
+// }
+
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized(); // Required
+  // await Firebase.initializeApp(); // ✅ MUST be awaited
+  // runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
